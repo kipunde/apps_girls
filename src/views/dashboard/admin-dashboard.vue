adce51d9a2ec52fdd8aa378d280bd646772cb22e
Dullayo Kipunde <abdallah.kipunde@gmail.com>
2026-03-12 17:52:39 +0300

    Revert "Update admin-dashboard.vue"
    This reverts commit 2d4bb5e689b679adaa4df9e726955180c194eaa4.

--- src/views/dashboard/admin-dashboard.vue
+++ src/views/dashboard/admin-dashboard.vue
@@ -85,10 +85,111 @@ async mounted() {
         </div>
 
         <!-- Charts & Recent Courses -->
-       
+        <div class="row mt-4">
+          <div class="col-xl-7 col-sm-12 col-12 d-flex">
+            <div class="card flex-fill">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <h5 class="card-title mb-0">Course Enrollments</h5>
+                <div class="graph-sets">
+                  <ul class="mb-0">
+                    <li><span>Enrollments</span></li>
+                    <li><span>Completed</span></li>
+                  </ul>
+                  <div class="dropdown dropdown-wraper">
+                    <button
+                      class="btn btn-light btn-sm dropdown-toggle"
+                      type="button"
+                      id="dropdownMenuButton"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      2026
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <li v-for="year in [2026,2025,2024]" :key="year">
+                        <a href="javascript:void(0);" class="dropdown-item">{{ year }}</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <apexchart
+                  type="bar"
+                  height="300"
+                  :options="basicAreaChart.sline"
+                  :series="basicAreaChart.series"
+                ></apexchart>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-5 col-sm-12 col-12 d-flex">
+            <div class="card flex-fill default-cover mb-4">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <h4 class="card-title mb-0">Recent Courses</h4>
+                <div class="view-all-link">
+                  <router-link to="/courses" class="view-all d-flex align-items-center">
+                    View All <vue-feather type="arrow-right" class="feather-16 ps-2"></vue-feather>
+                  </router-link>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive dataview">
+                  <table class="table dashboard-recent-products">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Course</th>
+                        <th>Instructor</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="course in recentCourses" :key="course.id">
+                        <td>{{ course.id }}</td>
+                        <td>{{ course.title }}</td>
+                        <td>{{ course.instructor }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <!-- Recent Enrollments -->
-        
+        <div class="card mt-4">
+          <div class="card-header">
+            <h4 class="card-title">Recent Enrollments</h4>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive dataview">
+              <table class="table dashboard-expired-products">
+                <thead>
+                  <tr>
+                    <th>User</th>
+                    <th>Course</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="enroll in recentEnrollments" :key="enroll.id">
+                    <td>{{ enroll.user }}</td>
+                    <td>{{ enroll.course }}</td>
+                    <td>{{ enroll.date }}</td>
+                    <td>
+                      <span :class="['badge', enroll.status === 'Active' ? 'bg-success' : 'bg-secondary']">
+                        {{ enroll.status }}
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
